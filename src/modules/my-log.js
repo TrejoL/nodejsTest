8a768d6b64bac08a6c901baf8574623f9c10da7b
Luis Trejo <aioria_t@hotmail.com>
2020-01-28 21:32:41 -0600

    init

---
+++ src/modules/my-log.js
@@ -0,0 +1,14 @@
+function info(text) {
+  console.log("INFO", text);
+  return text;
+}
+
+function error(text) {
+  console.log("ERROR", text);
+  return text;
+}
+//exportacion parcial
+module.exports.info = info;
+module.exports.error = error;
+//exportacion global
+//module.exports = { info, error };
